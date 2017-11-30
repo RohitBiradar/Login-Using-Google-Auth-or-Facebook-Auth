eae9ad0868b909cdf645079a6344210c43dfacce
ROHIT <rohitbiradar18@gmail.com>
2017-11-30 12:42:48 +0530

    config

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '902962386523138',
+		'clientSecret': 'f7f223bcfaa34ccad3ec4325ec81a8ac',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'googleAuth' : {
+		'clientID': '628605503715-7adifkcqhs9lv79h0cd604tm0cbiieg2.apps.googleusercontent.com',
+		'clientSecret': 'RhdFJNe7S1zQ_0y0uC755G6l',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+}
